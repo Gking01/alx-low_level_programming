be7e0dde5cf63e4e5d683f43201dc95d87d5e1c8
Isghak <isghakcollins9@gmail.com>
2022-06-02 09:32:45 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /*
- * discription: main - Prints out a string to stdout.
- * return:0 if successful
+ * main - Prints out a text
+ * return: always 0 if successful
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
