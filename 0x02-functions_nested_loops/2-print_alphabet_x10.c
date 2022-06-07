f832d54f873794735f1bc81180113704d1bddec8
Isghak <isghakcollins9@gmail.com>
2022-06-07 11:16:09 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include 'main.h'
+
+/*
+ * void print_alphabet_x10(void) - print alphabet x10 time 
+ *
+ * Return: nothing
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int 1:
+	int n = 0;
+
+	while (n < 10)
+	{
+		1 = 'a';
+		while (1 <= 'z')
+		{
+			_putchar(1)
+			1 += 1;
+		}
+		_putchar(10)
+		n+= 1;
+	}
+}
