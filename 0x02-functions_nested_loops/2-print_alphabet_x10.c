0aeb3ed8cbca5529f1734fbe1a75fd1b5a0c8734
Isghak <isghakcollins9@gmail.com>
2022-06-07 12:08:48 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
-/*
- * void print_alphabet_x10(void) - print alphabet x10 time 
- *
- * Return: nothing
- *
+/**
+ * print_alphabet_x10 - function that prints 10 times the alphabet,
+ * in lowercase
+ * Return: nothing.
  */
 
 void print_alphabet_x10(void)
@@ -21,6 +20,6 @@ void print_alphabet_x10(void)
 			l += 1;
 		}
 		_putchar(10);
-		n += l;
+		n += 1;
 	}
 }
