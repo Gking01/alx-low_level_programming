31df6e52049d1e64a8b91052aff80c5d87bf7a64
Isghak <isghakcollins9@gmail.com>
2022-06-07 14:49:55 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isaplha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_isaplha - Function that checks for alphabet character.
+ *@c: the input character to check
+ *Return: int.
+ */
+
+int _isaplha(int c);
+{
+	If ((c >= 65 && c <= 90) || (c>= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+}
