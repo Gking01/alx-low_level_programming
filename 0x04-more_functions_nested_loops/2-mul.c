1f57e12a9c142623fec9ecb5d9e496b7b83b1ac1
Isghak <isghakcollins9@gmail.com>
2022-06-09 08:32:02 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul(int a; int b) - funtion the multiplies two intergers.
+ * @a: first input value to check
+ * @b: second input value to check
+ * Return: int.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
