e3c2751d61d3197d3ee5146029106867c503374d
Isghak <isghakcollins9@gmail.com>
2022-06-09 11:05:12 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,17 @@
 
 int main(void)
 {
-    int i;
-    for(i=1; i<=100; ++i)
-    {
-        if (i % 3 == 0)
-            printf("Fizz");
-        if (i % 5 == 0)
-            printf("Buzz");
-        if ((i % 3 != 0) && (i % 5 != 0))
-            printf("number=%d", i);
-        printf("\n");
-    }
-
-    return 0;
+	int i;
+	for(i=1; i<=100; ++i)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if ((i % 3 != 0) && (i % 5 != 0))
+			printf("number=%d", i);
+		printf("\n");
+	}
+	
+	return 0;
 }
