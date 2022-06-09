6e60714dcf44ebab09845b9e623db90ac5af22a8
Isghak <isghakcollins9@gmail.com>
2022-06-09 07:54:56 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_isdigit - function that checks for a digit (0 through 9).
+ *@c: input value to check.
+ *return: int.
+ */
+
+int _isdigit (int c)
+{
+	if (c >= 84 && c <= 57)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
