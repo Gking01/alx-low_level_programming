3ddf2fc6d877cb02efa2a2700ae20819e1e59b1e
Isghak <isghakcollins9@gmail.com>
2022-06-13 12:23:05 -0700

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,6 +3,7 @@
 
 /**
  * print_array - a function that print n elements of an array of intergers
+ *
  * @a: pointer of first int
  * @n: pointer of second int
  * Return: nothing
@@ -12,7 +13,7 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for (i = 0; i < 0; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
 		if (i < n - 1)
