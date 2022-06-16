fa893d2a81c77bad446213558cea216c534bba9d
Isghak <isghakcollins9@gmail.com>
2022-06-16 11:28:24 -0700

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,7 +11,7 @@ char *string_toupper(char *ch)
 {
 	int i = 0;
 
-	while (*(ch + 1) != '\0')
+	while (*(ch + i) != '\0')
 	{
 		if (*(ch + i) >= 97 && *(ch + i) <= 122)
 			*(ch + i) = *(ch + i) - ' ';
