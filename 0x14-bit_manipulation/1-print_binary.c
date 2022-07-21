0eb8e9031b471aa0a894f5b780472e039ea28ceb
Isghak <isghakcollins9@gmail.com>
2022-07-21 13:24:35 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -37,19 +37,3 @@ void _divide(unsigned long int n)
 	else
 		_putchar('0');
 }
-
-int main(void)
-{
-    print_binary(0);
-    printf("\n");
-    print_binary(1);
-    printf("\n");
-    print_binary(98);
-    printf("\n");
-    print_binary(1024);
-    printf("\n");
-    print_binary((1 << 10) + 1);
-    printf("\n");
-    return (0);
-}
-
